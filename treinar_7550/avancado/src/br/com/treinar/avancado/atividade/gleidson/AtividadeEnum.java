fc1949caadad5dbc2bfadae9193f2518b07aa5fe
Gleidson Guimarães Moura [instrutor] <gleidson.gmoura@gmail.com>
2015-09-28 19:52:17 -0300

    Atividade Enum

---
+++ treinar_7550/avancado/src/br/com/treinar/avancado/atividade/gleidson/AtividadeEnum.java
@@ -0,0 +1,9 @@
+package br.com.treinar.avancado.atividade.gleidson;
+
+public class AtividadeEnum {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
--- treinar_7550/avancado/src/br/com/treinar/avancado/aula/TesteEnum.java
+++ treinar_7550/avancado/src/br/com/treinar/avancado/aula/TesteEnum.java
@@ -1,8 +1,39 @@
 package br.com.treinar.avancado.aula;
 
+import java.util.Scanner;
+
+import br.com.treinar.avancado.Sexo;
+
 public class TesteEnum {
 
 	public static void main(String[] args) {
+		
+		
+		Sexo s = Sexo.M;
+		
+		System.out.println(s.getDescricao());
+		
+		System.out.println("Digite: ");
+//		Devemos evitar codigos diretos
+//		System.out.println("0 - Feminino" );
+//		System.out.println("1 - Masculino" );
+		
+		Sexo[] sexos = Sexo.values(); 	
+		
+		
+		for (Sexo sexo : sexos) {
+			System.out.println(sexo + " - " + sexo.getDescricao());
+		}
+		
+		
+		System.out.println();
+		Scanner sc = new Scanner(System.in);
+		String sexoStr = sc.nextLine();
+		
+		Sexo escolhido = Sexo.valueOf(sexoStr);
+		
+		System.out.println(escolhido.getDescricao() + " posicao enum " + escolhido.ordinal());
+		sc.close();
 		
 	}
 	
