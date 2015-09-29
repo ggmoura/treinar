1eb6ca9af214be9be9c78bf3cd2fa0b3e4e74ed4
ADM <ADM@TRE-BHZ-008>
2015-09-28 21:51:33 -0300

    List Laila

---
+++ treinar_7550/avancado/src/AtividadeList.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AtividadeList {
+
+	public static void main(String[] args) {
+		
+	List a1 = new ArrayList();
+	a1.add("Laila");
+	a1.add("Marco");
+	a1.add("Marcos");
+	a1.add("Diego");
+	a1.add("Diego");
+	a1.add(27);
+	
+	
+	System.out.println(a1);
+
+	}
+
+}
