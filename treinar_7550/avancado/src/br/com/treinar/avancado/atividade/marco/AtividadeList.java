d5653ac958f08d66d2e0cd89599ff72fd60fedee
Marco Lopes <marco.flopes@hotmail.com>
2015-09-29 19:19:04 -0300

    Exercício 28/09

--- treinar_7550/avancado/src/br/com/treinar/avancado/atividade/marco/AtividadeList.java
+++ treinar_7550/avancado/src/br/com/treinar/avancado/atividade/marco/AtividadeList.java
@@ -7,9 +7,9 @@ public class AtividadeList {
 	public static void main(String[] args) {
 		List<String> a1 = new ArrayList<>();
 
-		a1.add("Natan");
-		a1.add("Neimar");
-		a1.add("Cruzeiro");
+		a1.add("João");
+		a1.add("Joaquim");
+		a1.add("José");
 
 		System.out.println(a1);
 
