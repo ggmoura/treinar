67082d945eab658b07e46d8617568bca76c1e32a
ADM <ADM@TRE-BHZ->
2015-09-28 21:53:28 -0300

    O oitavo

---
+++ treinar_7550/avancado/src/br/com/treinar/avancado/atividade/rodrigo/AtividadeEnum.java
@@ -0,0 +1,23 @@
+package br.com.treinar.avancado.atividade.rodrigo;
+
+import java.util.Scanner;
+
+import br.com.treinar.avancado.Sexo;
+
+public class AtividadeEnum {
+
+	public static void main(String[] args) {
+		Sexo s = Sexo.M;
+		Sexo c = Sexo.F;
+		
+		System.out.println("Digite 0");
+		System.out.println("Masculino");
+		System.out.println("1 \n Feminino");
+	Scanner sc = new Scanner(System.in);
+	System.out.println(s.getDescricao());
+	
+	
+	}
+
+	}
+	
---
+++ treinar_7550/avancado/src/br/com/treinar/avancado/atividade/rodrigo/AtividadeList.java
@@ -0,0 +1,23 @@
+package br.com.treinar.avancado.atividade.rodrigo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AtividadeList {
+
+	public static void main(String[] args) {
+
+		List<String> pilha = new ArrayList<>();
+		pilha.add("primeiro");
+		pilha.add("segundo");
+		pilha.add("terceiro");
+		pilha.add("quarto");
+		pilha.add("quinto");
+		pilha.add("Alien");
+		//pilha.add(7);
+		System.out.println(pilha);
+		System.out.println(7 + 1);
+
+	}
+
+}
