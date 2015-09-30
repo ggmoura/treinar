6369d7fe3f5a45b38f012c0c442e8cfb447dc27e
ADM <ADM@TRE-BHZ->
2015-09-30 19:50:56 -0300

    ordn

---
+++ treinar_7550/avancado/src/br/com/treinar/avancado/atividade/rodrigo/AtividadeMaps.java
@@ -0,0 +1,24 @@
+package br.com.treinar.avancado.atividade.rodrigo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import br.com.treinar.avancado.atividade.mariana.AtividadeMap;
+
+public class AtividadeMaps {
+
+	public static void main(String[] args) {
+
+		Map<Character, List<String>> mapa = AtividadeMap.recuperarMapaNomes();
+		
+		Set<Entry<Character, List<String>>> entrySet = mapa.entrySet();
+		for(Entry<Character, List<String>> entry : entrySet) {
+			System.out.println(entry.getKey() + " - " + entry.getValue());
+		
+		
+		} 
+		
+	}
+}
