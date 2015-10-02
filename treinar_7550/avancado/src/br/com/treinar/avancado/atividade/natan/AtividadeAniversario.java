c090fdfc227e473bbfafbd866b242ba884724091
ADM <ADM@TRE-BHZ-011>
2015-10-01 22:04:38 -0300

    ativ natan

---
+++ treinar_7550/avancado/src/br/com/treinar/avancado/atividade/natan/AtividadeAniversario.java
@@ -0,0 +1,20 @@
+package br.com.treinar.avancado.atividade.natan;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class AtividadeAniversario {
+
+	public static void main(String[] args) {
+		Calendar cal = Calendar.getInstance();
+
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		cal.set(Calendar.YEAR, 1992);
+		cal.set(Calendar.MONTH, 0);
+		
+		DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+		System.out.println(df.format(cal.getTime()));
+		
+	}
+}
---
+++ treinar_7550/avancado/src/br/com/treinar/avancado/atividade/natan/AtividadeGravaArquivo.java
@@ -0,0 +1,45 @@
+package br.com.treinar.avancado.atividade.natan;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.treinar.avancado.Pessoa;
+import br.com.treinar.avancado.Sexo;
+
+public class AtividadeGravaArquivo {
+
+	public static void main(String[] args) throws IOException {
+		Pessoa natan = new Pessoa("Natan", Sexo.M);
+		natan.setCpf(10677322224l);
+
+		Pessoa isabela = new Pessoa("isabela", Sexo.F);
+		isabela.setCpf(10020030040l);
+
+		Pessoa marco = new Pessoa("Marco", Sexo.M);
+		marco.setCpf(10020030060l);
+
+		List<Pessoa> alunos = new ArrayList<>();
+
+		alunos.add(natan);
+		alunos.add(isabela);
+		alunos.add(marco);
+
+		OutputStream os = new FileOutputStream("natan.txt");
+		OutputStreamWriter osw = new OutputStreamWriter(os);
+		BufferedWriter bw = new BufferedWriter(osw);
+
+		for (Pessoa pessoa : alunos) {
+			bw.write(pessoa.getNome() + ";" + pessoa.getCpf() + ";" + pessoa.getSexo().getDescricao() + ";");
+			bw.newLine();
+		}
+
+		bw.close();
+
+	}
+
+}
