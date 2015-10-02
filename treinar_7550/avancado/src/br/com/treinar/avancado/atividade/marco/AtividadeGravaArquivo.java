179de2aec8a1ad2607c3501d21ad4c5b09d2b6c4
Marco Lopes <marco.flopes@hotmail.com>
2015-10-02 18:59:47 -0300

    Atividade01/10

--- treinar_7550/avancado/src/br/com/treinar/avancado/atividade/marco/AtividadeGravaArquivo.java
+++ treinar_7550/avancado/src/br/com/treinar/avancado/atividade/marco/AtividadeGravaArquivo.java
@@ -14,29 +14,27 @@ import br.com.treinar.avancado.Sexo;
 public class AtividadeGravaArquivo {
 
 	public static void main(String[] args) throws IOException {
-		
-		Pessoa p = new Pessoa("Marco",Sexo.M);
-		Pessoa p1 = new Pessoa("João",Sexo.M);
-		Pessoa p2 = new Pessoa("Leila",Sexo.F);
-		
+
+		Pessoa p = new Pessoa("Marco", Sexo.M);
+		Pessoa p1 = new Pessoa("João", Sexo.M);
+		Pessoa p2 = new Pessoa("Leila", Sexo.F);
+
 		List<Pessoa> atividadeIO = new ArrayList<>();
 		atividadeIO.add(p);
 		atividadeIO.add(p1);
 		atividadeIO.add(p2);
-				
+
 		OutputStream os = new FileOutputStream("saidaMarco.txt");
 		OutputStreamWriter osw = new OutputStreamWriter(os);
 		BufferedWriter bw = new BufferedWriter(osw);
-		
+
 		for (Pessoa pessoa : atividadeIO) {
-			
-			bw.write(pessoa.getNome()+";"+pessoa.getSexo().ordinal());
+
+			bw.write(pessoa.getNome() + ";" + pessoa.getSexo().ordinal());
 			bw.newLine();
-			
-		
-			
+
 		}
-		
+
 		bw.close();
 	}
 
