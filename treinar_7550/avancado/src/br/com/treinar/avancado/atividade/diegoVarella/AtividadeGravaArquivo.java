f987f64240bfc0228eb09ec72924ff0a7cd35457
Diego Varella Rodrigues <diegovarella@gmail.com>
2015-10-02 19:10:20 -0300

    Atividade atualisada

--- treinar_7550/avancado/src/br/com/treinar/avancado/atividade/diegoVarella/AtividadeGravaArquivo.java
+++ treinar_7550/avancado/src/br/com/treinar/avancado/atividade/diegoVarella/AtividadeGravaArquivo.java
@@ -5,6 +5,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.treinar.avancado.Pessoa;
 import br.com.treinar.avancado.Sexo;
@@ -12,25 +14,30 @@ import br.com.treinar.avancado.Sexo;
 public class AtividadeGravaArquivo {
 
 	public static void main(String[] args) throws IOException {
-		Pessoa p1 = new Pessoa("Diego", Sexo.M, 1l);
-		Pessoa p2 = new Pessoa("Prscila", Sexo.F, 4l);
-		Pessoa p3 = new Pessoa("Rodrigo", Sexo.M, 2l);
-		Pessoa p4 = new Pessoa("Bianca", Sexo.F, 3l);
+
+		Pessoa pessoa1 = new Pessoa("Diego", Sexo.M, 4l);
+		Pessoa pessoa2 = new Pessoa("Priscila", Sexo.F, 3l);
+		Pessoa pessoa3 = new Pessoa("Rodrigo", Sexo.M, 2l);
+		Pessoa pessoa4 = new Pessoa("Bianca", Sexo.F, 1l);
+
+		List<Pessoa> listaPessoas = new ArrayList<>();
+
+		listaPessoas.add(pessoa1);
+		listaPessoas.add(pessoa2);
+		listaPessoas.add(pessoa3);
+		listaPessoas.add(pessoa4);
 
 		OutputStream os = new FileOutputStream("diegoVarellaArquivo.txt");
 		OutputStreamWriter osw = new OutputStreamWriter(os);
 		BufferedWriter bw = new BufferedWriter(osw);
-		
-		bw.write("Lista de Pessoas");
+
+		bw.write("Lista de pessoas");
 		bw.newLine();
-		bw.write(p1.toString());
-		bw.newLine();
-		bw.write(p2.toString());
-		bw.newLine();
-		bw.write(p3.toString());
-		bw.newLine();
-		bw.write(p4.toString());
-	
+
+		for (Pessoa pessoa : listaPessoas) {
+			bw.write(pessoa.getNome() + ";" + pessoa.getCpf() + ";" + pessoa.getSexo().ordinal() + ";");
+			bw.newLine();
+		}
 
 		bw.close();
 
