dd85ca17483dc498e42e71704aaf98dbff0b1427
Diego Varella <diegovarella@gmail.com>
2015-10-08 19:53:27 -0300

    Arquivo HTML

--- treinar_7550/agenda-web-diegovarella/src/br/com/treinar/agenda/PrimeiroServlet.java
+++ treinar_7550/agenda-web-diegovarella/src/br/com/treinar/agenda/PrimeiroServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class PrimeiroServlet
  */
-@WebServlet({ "/PrimeiroServlet", "/olamundo" })
+@WebServlet({ "/PrimeiroServlet", "/olamundo", "*.treinar" })
 public class PrimeiroServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
