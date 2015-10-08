0522640885794d8834e98476ac72ab2a284b5325
Diego Varella <diegovarella@gmail.com>
2015-10-07 22:14:26 -0300

    Diego Varella Rodrigues

--- treinar_7550/diegovarella-jdbc/src/teste/TestaConnection.java
+++ treinar_7550/diegovarella-jdbc/src/teste/TestaConnection.java
@@ -3,24 +3,21 @@ package teste;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-
 public class TestaConnection {
 
 	public static void main(String[] args) {
-		
+
 		try {
-			
+
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbagenda", "root", "");
-			System.out.println("Conexão com o banco de dados estabelecida com sucesso ao bando " + con.getCatalog());
-			
+			System.out.println("Conexão com o banco de dados estabelecida com sucesso ao banco " + con.getCatalog());
+
 		} catch (Exception e) {
-			
+
 			System.err.println("Falha ao se conectar com o banco de dados.");
-			
+
 		}
 
-		
-		
 	}
 
 }
