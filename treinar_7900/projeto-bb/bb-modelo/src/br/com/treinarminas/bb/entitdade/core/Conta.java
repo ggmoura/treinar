63352248023e23a774a77ead8ab768d36cff1550
gleidson <gleidson.gmoura@gmail.com>
2016-01-06 20:57:58 -0200

    Signed-off-by: gleidson <gleidson.gmoura@gmail.com>

--- treinar_7900/projeto-bb/bb-modelo/src/br/com/treinarminas/bb/entitdade/core/Conta.java
+++ treinar_7900/projeto-bb/bb-modelo/src/br/com/treinarminas/bb/entitdade/core/Conta.java
@@ -2,7 +2,15 @@ package br.com.treinarminas.bb.entitdade.core;
 
 public class Conta {
 
-	private String nomeCorrentista;
-	double saldo;
+	public String nomeCorrentista;
+	private double saldo;
+	public double limiteCredito;
 	
+	public void depositar(double valor) {
+		saldo += valor;
+	}
+	
+	public double recuperarSaldo() {
+		return saldo + limiteCredito;
+	}
 }
