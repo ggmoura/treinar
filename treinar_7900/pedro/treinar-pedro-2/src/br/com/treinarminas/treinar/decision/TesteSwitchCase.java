1b385580fd72542be29f0160df53f87dd9d18691
pedrofiuza <pedrofiuza1992@gmail.com>
2016-01-06 20:19:15 -0300

    projeto dois

---
+++ treinar_7900/pedro/treinar-pedro-2/src/br/com/treinarminas/treinar/decision/TesteSwitchCase.java
@@ -0,0 +1,55 @@
+package br.com.treinarminas.treinar.decision;
+
+import java.util.Scanner;
+
+public class TesteSwitchCase {
+
+	public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.print("Informe a opção: ");
+		
+		int opcao = leitor.nextInt();
+		
+		//libera recursos do SO (Sistema Operacional)
+		leitor.close();
+		
+		switch (opcao) {
+		case 1:
+			System.out.println("Opção Um");
+			//break;
+		case 2:
+			System.out.println("Opção Dois");
+			break;
+		case 3:
+			System.out.println("Opção Três");
+			break;
+		case 4:
+			System.out.println("Opção Quatro");
+			break;
+
+		default:
+			System.out.println("Opção inválida");
+			break;
+		}
+		
+		
+		String nome = "gleidson";
+		//pode ser usado com string a partir da versao seis
+		switch (nome) {
+		case "maria sophia":
+			
+			break;
+		case "gleidson":
+			
+			break;
+
+		default:
+			break;
+		}
+		
+		
+	}
+	
+}
---
+++ treinar_7900/pedro/treinar-pedro-2/src/br/com/treinarminas/treinar/metodo/Pessoa.java
@@ -0,0 +1,17 @@
+package br.com.treinarminas.treinar.metodo;
+
+public class Pessoa {
+
+	int idade;
+	
+	String nome;
+	
+	void correr() {
+		System.out.println("correndo");
+	}
+	
+	void envelhecer() {
+		idade++;
+	}
+	
+}
