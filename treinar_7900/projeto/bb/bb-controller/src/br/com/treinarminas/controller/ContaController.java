6cd1da54bdb0967df8e73fbfa5d9382b156de2d0
gleidson <gleidson.gmoura@gmail.com>
2016-01-07 22:29:03 -0200

    identacao

--- treinar_7900/projeto/bb/bb-controller/src/br/com/treinarminas/controller/ContaController.java
+++ treinar_7900/projeto/bb/bb-controller/src/br/com/treinarminas/controller/ContaController.java
@@ -5,12 +5,12 @@ import br.com.treinarminas.bb.entitdade.core.Conta;
 public class ContaController {
 
 	private Conta conta;
-	
+
 	public void cadastrarConta(Conta conta) {
-		//o operador this faz referencia ao atributo da classe
+		// o operador this faz referencia ao atributo da classe
 		this.conta = conta;
 	}
-	
+
 	public double recuperarSaldo() {
 		return conta.recuperarSaldo();
 	}
@@ -18,5 +18,5 @@ public class ContaController {
 	public Boolean sacar(Double valor) {
 		return conta.sacar(valor);
 	}
-	
+
 }
