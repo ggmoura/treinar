59fffb99f2c2828445b7f09dd3252c5a0fc7399a
Wells <Wells@172.16.99.184>
2016-01-07 22:25:55 -0200

    ve

---
+++ treinar_7900/wellington/treinar-wellington/src/loops/Veiculo.java
@@ -0,0 +1,42 @@
+package loops;
+
+public class Veiculo {
+
+	private String modelo;
+	private String marca;
+	private Integer placa;
+	private Integer ano;
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public Integer getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(Integer placa) {
+		this.placa = placa;
+	}
+
+	public Integer getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+
+}
