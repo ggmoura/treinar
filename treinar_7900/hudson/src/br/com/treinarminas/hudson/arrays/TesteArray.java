34640c839ba7b7fa187b65624f532ca4a4c5d1e6
ADM <ADM@tre-bhz-94>
2016-01-13 19:50:59 -0200

    Teste

--- treinar_7900/hudson/src/br/com/treinarminas/hudson/arrays/TesteArray.java
+++ treinar_7900/hudson/src/br/com/treinarminas/hudson/arrays/TesteArray.java
@@ -7,14 +7,14 @@ public class TesteArray {
 	public static void main(String[] args) {
 
 		String[] alunos;
-		
+
 		String qtdAlunosStr = JOptionPane
 				.showInputDialog("Informe a quantidade de alunos presentes");
 
 		Integer qtdAlunos = Integer.valueOf(qtdAlunosStr);
-		
+
 		alunos = new String[qtdAlunos];
-		
+
 		for (int i = 0; i < alunos.length; i++) {
 			alunos[i] = JOptionPane.showInputDialog("Aluno " + (i + 1));
 		}
@@ -22,7 +22,7 @@ public class TesteArray {
 		for (int j = 0; j < alunos.length; j++) {
 			str.append("Aluno " + (j + 1) + " - Nome: " + alunos[j] + "\n");
 		}
-		
+
 		JOptionPane.showMessageDialog(null, str);
 
 	}
