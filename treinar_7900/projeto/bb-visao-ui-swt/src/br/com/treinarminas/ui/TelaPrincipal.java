2f8b68ca44d339ef08b357e8362b730b03dc14dc
Usuario <Usuario@SL05PROFESSOR>
2016-01-15 18:58:55 -0200

    Caso de uso alterar taxa de rendimento

--- treinar_7900/projeto/bb-visao-ui-swt/src/br/com/treinarminas/ui/TelaPrincipal.java
+++ treinar_7900/projeto/bb-visao-ui-swt/src/br/com/treinarminas/ui/TelaPrincipal.java
@@ -7,6 +7,10 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormAttachment;
 
 public class TelaPrincipal {
 
@@ -47,18 +51,48 @@ public class TelaPrincipal {
 		shell = new Shell();
 		shell.setSize(450, 300);
 		shell.setText("SWT Application");
+		shell.setLayout(new FormLayout());
 		
 		ContaCorrente contaCorrente = new ContaCorrente(shell, SWT.NONE);
-		contaCorrente.setBounds(0, 0, 434, 261);
+		FormData fd_contaCorrente = new FormData();
+		fd_contaCorrente.bottom = new FormAttachment(0, 241);
+		fd_contaCorrente.right = new FormAttachment(0, 434);
+		fd_contaCorrente.top = new FormAttachment(0);
+		fd_contaCorrente.left = new FormAttachment(0);
+		contaCorrente.setLayoutData(fd_contaCorrente);
+		contaCorrente.setLayout(new FormLayout());
 		
 		ContaPoupanca contaPoupanca = new ContaPoupanca(shell, SWT.NONE);
-		contaPoupanca.setBounds(0, 0, 434, 261);
+		FormData fd_contaPoupanca = new FormData();
+		fd_contaPoupanca.bottom = new FormAttachment(0, 261);
+		fd_contaPoupanca.right = new FormAttachment(0, 434);
+		fd_contaPoupanca.top = new FormAttachment(0);
+		fd_contaPoupanca.left = new FormAttachment(0);
+		contaPoupanca.setLayoutData(fd_contaPoupanca);
 		
 		ContaSalario contaSalario = new ContaSalario(shell, SWT.NONE);
-		contaSalario.setBounds(0, 0, 434, 261);
+		FormData fd_contaSalario = new FormData();
+		fd_contaSalario.bottom = new FormAttachment(0, 261);
+		fd_contaSalario.right = new FormAttachment(0, 434);
+		fd_contaSalario.top = new FormAttachment(0);
+		fd_contaSalario.left = new FormAttachment(0);
+		contaSalario.setLayoutData(fd_contaSalario);
 		
 		ContaInvestimento contaInvestimento = new ContaInvestimento(shell, SWT.NONE);
-		contaInvestimento.setBounds(0, 0, 424, 251);
+		FormData fd_contaInvestimento = new FormData();
+		fd_contaInvestimento.bottom = new FormAttachment(0, 251);
+		fd_contaInvestimento.right = new FormAttachment(0, 424);
+		fd_contaInvestimento.top = new FormAttachment(0);
+		fd_contaInvestimento.left = new FormAttachment(0);
+		contaInvestimento.setLayoutData(fd_contaInvestimento);
+
+		TelaCadastroTaxaRendimento compositeTaxarendimento = new TelaCadastroTaxaRendimento(shell, SWT.NONE);
+		FormData fd_compositeTaxarendimento = new FormData();
+		fd_compositeTaxarendimento.bottom = new FormAttachment(0, 241);
+		fd_compositeTaxarendimento.right = new FormAttachment(0, 434);
+		fd_compositeTaxarendimento.top = new FormAttachment(0);
+		fd_compositeTaxarendimento.left = new FormAttachment(0);
+		compositeTaxarendimento.setLayoutData(fd_compositeTaxarendimento);
 		
 		Menu menu = new Menu(shell, SWT.BAR);
 		shell.setMenuBar(menu);
@@ -131,6 +165,26 @@ public class TelaPrincipal {
 		
 		MenuItem mntmListarContas = new MenuItem(menu_2, SWT.NONE);
 		mntmListarContas.setText("Listar Contas");
+		
+		MenuItem mntmBanco = new MenuItem(menu, SWT.CASCADE);
+		mntmBanco.setText("Banco");
+		
+		Menu menu_3 = new Menu(mntmBanco);
+		mntmBanco.setMenu(menu_3);
+		
+		MenuItem mntmTaxaRendimento = new MenuItem(menu_3, SWT.NONE);
+		mntmTaxaRendimento.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				contaCorrente.setVisible(Boolean.FALSE);
+				contaPoupanca.setVisible(Boolean.FALSE);
+				contaSalario.setVisible(Boolean.FALSE);
+				contaInvestimento.setVisible(Boolean.FALSE);
+				compositeTaxarendimento.setVisible(Boolean.TRUE);
+			}
+		});
+		mntmTaxaRendimento.setText("Taxa Rendimento");
+		
 
 	}
 }
