cd00a9ffab40eb07a852f8130ebaf3533b87ad5d
Jader Almeida <contato@jaderalmeida.com.br>
2016-01-19 21:50:07 -0200

    Enum

--- treinar_7900/jader/treinar-jader/src/br/treinarminas/treinar/ExerciseCollections/ExerciseCollections.java
+++ treinar_7900/jader/treinar-jader/src/br/treinarminas/treinar/ExerciseCollections/ExerciseCollections.java
@@ -36,11 +36,32 @@ public class ExerciseCollections {
 		} while (!nome.equalsIgnoreCase("0"));
 		s.close();
 		
+		System.out.println(mapNomes);
+		
 		for (Character key : mapNomes.keySet()) {
 			System.out.println(key);
 			System.out.println("---------------- \n");
 			List<String> imprimeNome = mapNomes.get(key + "\n");
 			System.out.println(imprimeNome);
 		}
+		/*
+		//recupera todas as chaves do mapa
+		Set<Character> keys = mapNomes.keySet();
+		for (Character key : keys) {
+			System.out.println(key);
+		}
+		System.out.println("\n\n");
+		//recupera todos os valores do mapa
+		Collection<List<String>> values = mapNomes.values();
+		
+		//para percorrer todos os elementos do map navegando em um objeto chave valor
+		Set<Entry<Character, List<String>>> entrys = mapNomes.entrySet();
+		
+		for (Entry<Character, List<String>> entry : entrys) {
+			System.out.print(entry.getKey());
+			System.out.print(" ");
+			System.out.println(entry.getValue());
+		}*/
+		
 	}
 }
