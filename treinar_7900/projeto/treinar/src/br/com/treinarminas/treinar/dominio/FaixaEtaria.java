4fb0563c0cec0148a5926b553592dcf49b3df737
Usuario <Usuario@SL05PROFESSOR>
2016-01-19 21:49:35 -0200

    , String descricao

--- treinar_7900/projeto/treinar/src/br/com/treinarminas/treinar/dominio/FaixaEtaria.java
+++ treinar_7900/projeto/treinar/src/br/com/treinarminas/treinar/dominio/FaixaEtaria.java
@@ -2,6 +2,53 @@ package br.com.treinarminas.treinar.dominio;
 
 public enum FaixaEtaria {
 
-	INFANTIL, ADOLECENTE, ADULTO, IDOSO;
-	
+	INFANTIL(0, 12, "Infantil"), 
+	ADOLECENTE(13, 18, "Adolecente"), 
+	ADULTO(19, 65, "Adulto"), 
+	IDOSO(65, "Idoso");
+
+	private Integer idadeMinima;
+	private Integer idadeMaxima;
+	private String descricao;
+
+	private FaixaEtaria(Integer idadeMinina, Integer idadeMaxima, String descricao) {
+		this(idadeMinina, descricao);
+		this.idadeMaxima = idadeMaxima;
+	}
+
+	private FaixaEtaria(Integer idadeMinina, String descricao) {
+		this.idadeMinima = idadeMinina;
+		this.descricao = descricao;
+	}
+
+	public Integer getIdadeMaxima() {
+		return idadeMaxima;
+	}
+
+	public Integer getIdadeMinima() {
+		return idadeMinima;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public static FaixaEtaria recuperarPorDescricao(String descricao) {
+		
+		FaixaEtaria recuperado = null;
+		
+		for (FaixaEtaria faixa : values()) {
+			if (faixa.getDescricao().equals(descricao)) {
+				recuperado = faixa;
+				break;
+			}
+		}
+		
+		if (recuperado != null) {
+			return recuperado;
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
+
 }
--- treinar_7900/projeto/treinar/src/br/com/treinarminas/treinar/dominio/TesteEnum.java
+++ treinar_7900/projeto/treinar/src/br/com/treinarminas/treinar/dominio/TesteEnum.java
@@ -13,8 +13,23 @@ public class TesteEnum {
 		FaixaEtaria[] faixas = FaixaEtaria.values();
 		
 		for (FaixaEtaria faixaEtaria : faixas) {
-			System.out.println(faixaEtaria);
+			System.out.println(faixaEtaria.getDescricao());
 		}
+		
+		
+		String _enum = "INFANTIL";
+		
+		FaixaEtaria infantil = FaixaEtaria.valueOf(_enum);
+		
+		System.out.println(infantil.getIdadeMinima());
+		System.out.println(infantil.getIdadeMaxima());
+		
+		String descricao = "Infantil";
+		
+		FaixaEtaria recuperadoPorDescricao = FaixaEtaria.recuperarPorDescricao(descricao);
+		System.out.println(recuperadoPorDescricao.getIdadeMinima());
+		System.out.println(recuperadoPorDescricao.getIdadeMaxima());
+		
 		
 	}
 	
