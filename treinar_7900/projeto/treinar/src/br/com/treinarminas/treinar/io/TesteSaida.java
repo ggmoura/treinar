31ab76061936beb927b436e10120503d7d401d07
Usuario <Usuario@SL05PROFESSOR>
2016-01-20 21:39:49 -0200

    comparacao string com acento

---
+++ treinar_7900/projeto/treinar/src/br/com/treinarminas/treinar/io/TesteSaida.java
@@ -0,0 +1,21 @@
+package br.com.treinarminas.treinar.io;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class TesteSaida {
+
+	public static void main(String[] args) throws IOException {
+		OutputStream os = new FileOutputStream("saida.txt");
+		OutputStreamWriter osw = new OutputStreamWriter(os);
+		BufferedWriter bw = new BufferedWriter(osw);
+
+		bw.write("treinar");
+
+		bw.close();
+	}
+
+}
