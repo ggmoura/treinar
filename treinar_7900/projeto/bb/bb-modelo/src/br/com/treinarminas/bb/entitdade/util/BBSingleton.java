6fa764976c03a549cd3855d57eb786ec28026cce
Usuario <Usuario@SL05PROFESSOR>
2016-01-21 19:47:33 -0200

    recuperar contas

--- treinar_7900/projeto/bb/bb-modelo/src/br/com/treinarminas/bb/entitdade/util/BBSingleton.java
+++ treinar_7900/projeto/bb/bb-modelo/src/br/com/treinarminas/bb/entitdade/util/BBSingleton.java
@@ -13,6 +13,11 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.treinarminas.bb.entitdade.Cliente;
+import br.com.treinarminas.bb.entitdade.ContaCorrente;
+import br.com.treinarminas.bb.entitdade.ContaInvestimento;
+import br.com.treinarminas.bb.entitdade.ContaPoupanca;
+import br.com.treinarminas.bb.entitdade.ContaSalario;
 import br.com.treinarminas.bb.entitdade.core.Conta;
 
 public class BBSingleton {
@@ -29,6 +34,11 @@ public class BBSingleton {
 	private BBSingleton() {
 		super();
 		contas = new ArrayList<>();
+		try {
+			carregarContas();
+		} catch (Exception e) {
+			System.out.println(Constante.MENSAGEM_ERRO_CARREGAR_CONTAS);
+		}
 	}
 	
 	public static BBSingleton getInstance() {
@@ -51,32 +61,80 @@ public class BBSingleton {
 		contas.remove(numeroConta);
 	}
 	
-	public void carregarContas() throws IOException {
+	public void carregarContas() throws Exception {
 		
 		File f = new File("contas.txt");
+		Conta c = null;
 		
 		if (!f.exists()) {
 			f.createNewFile();
-		}
-		
-		InputStream is = new FileInputStream(f);
-		InputStreamReader isr = new InputStreamReader(is);
-		BufferedReader br = new BufferedReader(isr);
-		
-		String conta = br.readLine(); // primeira linha
-
-		while (conta != null) {
+		} else {
+			InputStream is = new FileInputStream(f);
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
 			
-			conta = br.readLine();					
+			String conta = br.readLine(); // primeira linha
+			String[] camposConta = conta.split(";");
+			ContaPoupanca.setTaxaRendimento(Double.parseDouble(camposConta[0]));
+			String clazz = null;
+			conta = br.readLine(); // primeira linha
+			while (conta != null) {
+				camposConta = conta.split(";");
+				clazz = camposConta[camposConta.length - 1];
+				c = (Conta) Class.forName(clazz).newInstance();
+				if (c instanceof ContaCorrente) {
+					preencherDadosConta((ContaCorrente)c, camposConta);
+				} else if (c instanceof ContaPoupanca) {
+					preencherDadosConta((ContaPoupanca)c, camposConta);
+				} else if (c instanceof ContaSalario) {
+					preencherDadosConta((ContaSalario)c, camposConta);					
+				} else if (c instanceof ContaInvestimento) {
+					preencherDadosConta((ContaInvestimento)c, camposConta);										
+				}
+				contas.add(c);
+				conta = br.readLine();					
+			}
+			br.close();
 		}
-		br.close();
+		
+	}
+	
+	public void preencherDadosConta(Conta conta, String[] dadosConta) {
+		conta.setCliente(new Cliente());
+		conta.getCliente().setNome(dadosConta[0]);
+		conta.getCliente().setCpf(!dadosConta[1].equals(Constante.STRING_NULL) ? Long.parseLong(dadosConta[1]) : null);
+		conta.setNumeroConta(Integer.parseInt(dadosConta[2]));
+		conta.setSaldo(Double.parseDouble(dadosConta[3]));
+	}
+	
+	
+	public void preencherDadosConta(ContaCorrente conta, String[] dadosConta) {
+		preencherDadosConta((Conta)conta, dadosConta);
+		conta.setLimiteCreditoDisponivel(Double.parseDouble(dadosConta[4]));
+		conta.setLimiteCreditoUtilizado(Double.parseDouble(dadosConta[5]));
+		conta.setTaxaManutencao(Double.parseDouble(dadosConta[6]));
+	}
+	
+	public void preencherDadosConta(ContaPoupanca conta, String[] dadosConta) {
+		preencherDadosConta((Conta)conta, dadosConta);
+	}
+	
+	public void preencherDadosConta(ContaSalario conta, String[] dadosConta) {
+		preencherDadosConta((Conta)conta, dadosConta);
+	}
+	
+	public void preencherDadosConta(ContaInvestimento conta, String[] dadosConta) {
+		preencherDadosConta((Conta)conta, dadosConta);
+		conta.setTaxaManutencao(Double.parseDouble(dadosConta[4]));
+		conta.setTaxaRendimento(Double.parseDouble(dadosConta[5]));
 	}
 	
 	public void gravarContas() throws IOException {
 		OutputStream os = new FileOutputStream("contas.txt");
 		OutputStreamWriter osw = new OutputStreamWriter(os);
 		BufferedWriter bw = new BufferedWriter(osw);
-		
+		bw.write(ContaPoupanca.getTaxaRendimento().toString());
+		bw.newLine();
 		for (Conta conta : contas) {
 			bw.write(conta.csv());
 			bw.newLine();
--- treinar_7900/projeto/bb/bb-modelo/src/br/com/treinarminas/bb/entitdade/util/Constante.java
+++ treinar_7900/projeto/bb/bb-modelo/src/br/com/treinarminas/bb/entitdade/util/Constante.java
@@ -5,5 +5,7 @@ public interface Constante {
 	Integer QUANTIDADE_CONTAS = 10;
 	String STRING_VAZIA = "";
 	String MENSAGEM_ERRO_GRAVAR_CONTAS = "Não foi possível gravar as contas. Comunique o analista responsável!";
+	String MENSAGEM_ERRO_CARREGAR_CONTAS = "Não foi possível carregar as contas. Comunique o analista responsável";
+	String STRING_NULL = "null";
 
 }
